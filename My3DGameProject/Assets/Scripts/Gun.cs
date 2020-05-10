d86c6255ff8f3a1e9cf5613f41ed54fb0a28b042
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-11 00:25:03 +0100

    Shooting/Explosions implemented

---
+++ My3DGameProject/Assets/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+  
+        
+    }
+
+    void Shoot()
+    {
+
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+}
