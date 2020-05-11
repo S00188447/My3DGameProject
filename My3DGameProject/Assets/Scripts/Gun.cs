b96ad2afb15f23ad32481c215964c208cb06003b
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-12 00:38:49 +0100

    Weapons added. Reloading/Ammo/Animations working

--- My3DGameProject/Assets/Scripts/Gun.cs
+++ My3DGameProject/Assets/Scripts/Gun.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -8,16 +8,41 @@ public class Gun : MonoBehaviour
     public float fireRate = 1f;
     public float impactForce = 30f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 1f;
 
+    public Animator animator;
 
+     void Start()
+    {
 
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+        
+    }
     void Update()
     {
+        if (isReloading)
+            return;
+
+        if(currentAmmo <0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if(Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 12f / fireRate;
@@ -27,9 +52,27 @@ public class Gun : MonoBehaviour
         
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading....");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime -.25f);//pauses for the reload time
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(0.25f);
+
+        currentAmmo = maxAmmo;
+
+        isReloading = false;
+    }
+
     void Shoot()
     {
         muzzleFlash.Play();
+
+        currentAmmo--;
 
         RaycastHit hit;
 
