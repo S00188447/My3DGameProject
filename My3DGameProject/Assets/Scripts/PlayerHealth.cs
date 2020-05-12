5f4c9fff02d0c390cc74a5e505798e5518ed4545
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-12 09:46:51 +0100

    Implemented Healthbar

---
+++ My3DGameProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    public Text text;
+    string message = "You Died";
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(20); //just to test healthbar
+        }
+
+        if(currentHealth <= 0)
+        {
+            text.text = message;         
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.setMaxHealth(currentHealth);
+    }
+}
