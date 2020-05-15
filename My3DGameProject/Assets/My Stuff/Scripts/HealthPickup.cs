0252a1991bcdeb628dc864f41316f78f36f3d257
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-15 22:41:14 +0100

    Implemented Health pickups and final boss fight. Project finished.
    There are two scenes and the game is preferred to be played in fullscreen. UI fits better

---
+++ My3DGameProject/Assets/My Stuff/Scripts/HealthPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : PlayerHealth
+{
+    public int healthAmount = 20;
+    private bool pickup = false;
+
+
+    void Update()
+    {
+        if (pickup == true)
+            currentHealth += healthAmount;               
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pickup = true;
+            Destroy(collision.gameObject);
+
+        }
+        
+    }
+}
