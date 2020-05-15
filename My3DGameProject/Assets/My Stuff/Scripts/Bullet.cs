ca89cbfc26392bf9711fe3c63874ef2d46dec9f5
S00188447 <47103333+PoxyZoxy@users.noreply.github.com>
2020-05-15 19:03:45 +0100

    Turret Enemy Implemented
    Turrets rotates to player when player comes in range and shoots until player exits range. Bullet has an impact effect. Created myself. The other 2 impact effects i didn't

---
+++ My3DGameProject/Assets/My Stuff/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+    public float bulletDamage = 50f;
+    public GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+   
+    void Update()
+    {
+        //if(target == null)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        //normalizing makes it a constant speed
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 2f);
+
+        //player takes damage
+        Destroy(gameObject);
+        
+    }
+}
